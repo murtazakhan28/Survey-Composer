7e1aa0e0ec81de2c94fa8a1574bc1090dd9a51de
Murtaza Khan <murtaza.khan1995@gmail.com>
2021-03-18 22:24:14 +0500

    fix for component constructor type error

--- src/components/layouts/mainLayout.tsx
+++ src/components/layouts/mainLayout.tsx
@@ -1,22 +1,16 @@
-import React, { createElement } from "react";
 import { Fragment } from "react";
 import { Route, RouteProps } from "react-router-dom";
 import { Navbar } from "../core";
 import "./index.css";
 
-const MainLayout = ({ component, ...rest }: RouteProps) => {
+const MainLayout = ({ component: Component, ...rest }: RouteProps) => {
 	return (
 		<Route
 			render={renderProps => {
 				return (
 					<Fragment>
 						<Navbar />
-						<div className="container">{/* {createElement(
-								() => (
-									<component />
-								),
-								renderProps
-							)} */}</div>
+						<div className="container">{Component ? <Component {...renderProps} /> : null}</div>
 					</Fragment>
 				);
 			}}
