dfd5b23d1ea19cdaa46d001bde4723e73b8d0082
Murtaza Khan <murtaza.khan1995@gmail.com>
2021-03-19 11:08:13 +0500

    added common text input component

---
+++ src/components/common/textInput/textInput.tsx
@@ -0,0 +1,15 @@
+import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+import "./index.css";
+
+interface TextInputProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+	value: string;
+	onChange: (value: any) => void;
+}
+
+const TextInput = ({ value, onChange, ...rest }: TextInputProps) => {
+	return (
+		<input type="text" className="text-input" value={value} onChange={e => onChange(e.target.value)} {...rest} />
+	);
+};
+
+export default TextInput;
--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -1,15 +1,23 @@
 import { Component } from "react";
 import { Survey } from "../../models";
+import { TextInput } from "../common";
 import "./index.css";
 
 class Editor extends Component {
-	state: { survey: Survey } = { survey: new Survey("TEST", "My First Survey", []) };
+	state: { survey: Survey; value: string } = {
+		survey: new Survey("TEST", "My First Survey", []),
+		value: ""
+	};
+
+	handleChange = (value: string) => this.setState({ value });
 
 	render() {
 		return (
 			<div className="container">
 				<div className="left-container bg-secondary border-sm"></div>
-				<div className="middle-container"></div>
+				<div className="middle-container">
+					<TextInput value={this.state.value} onChange={this.handleChange} placeholder="Placeholder" />
+				</div>
 			</div>
 		);
 	}
